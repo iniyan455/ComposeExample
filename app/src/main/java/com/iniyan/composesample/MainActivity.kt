1944b39aaef81d749aee4a321dfd9d5c36fa31df
Iniyan <Iniyan Rapido>
2021-12-25 22:09:47 +0530

    Card Layout Impl

--- app/src/main/java/com/iniyan/composesample/MainActivity.kt
+++ app/src/main/java/com/iniyan/composesample/MainActivity.kt
@@ -3,50 +3,100 @@ package com.iniyan.composesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import com.iniyan.composesample.ui.theme.ComposeSampleTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth()
-                    .border(5.dp, Color.Magenta)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Blue)
-                    .padding(5.dp)
-            ) {
-                Text(text = "Iniyan", modifier = Modifier
-                    .border(5.dp,Color.Yellow)
-                    .padding(5.dp)
-                    .offset(20.dp,20.dp)
-                    .border(10.dp,Color.Blue)
-                    .padding(10.dp)
-                )
-                Spacer(modifier = Modifier.height(50.dp))
-                Text(text = "Hello Iniyan",Modifier.clickable {  })
-//                Text(text = "Hello Scrollable",Modifier.scrollable{ })
-//                Text(text = "Hello Draggable",Modifier.draggable() {  })
+//            Column(
+//                modifier = Modifier
+//                    .background(Color.Green)
+//                    .fillMaxHeight(0.5f)
+//                    .fillMaxWidth()
+//                    .border(5.dp, Color.Magenta)
+//                    .padding(5.dp)
+//                    .border(5.dp, Color.Blue)
+//                    .padding(5.dp)
+//            ) {
+//                Text(text = "Iniyan", modifier = Modifier
+//                    .border(5.dp, Color.Yellow)
+//                    .padding(5.dp)
+//                    .offset(20.dp, 20.dp)
+//                    .border(10.dp, Color.Blue)
+//                    .padding(10.dp)
+//                )
+//                Spacer(modifier = Modifier.height(50.dp))
+//                Text(text = "Hello Iniyan",Modifier.clickable {  })
+////                Text(text = "Hello Scrollable",Modifier.scrollable{ })
+////                Text(text = "Hello Draggable",Modifier.draggable() {  })
+//            }
+
+            val painter = painterResource(id = R.drawable.ee)
+            val description = "Hohoodddddddooo"
+            val title = "ddddddddddd"
+            Box(modifier = Modifier.fillMaxWidth(0.5f)
+                .padding(16.dp)) {
+                ImageCard(painter = painter, contentDescription = description, title = title)
             }
-            
         }
     }
 }
+
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(colors = listOf(Color.Transparent,Color.Black),
+                    startY = 250f)
+                ))
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(
+                    text = title,
+                    style = TextStyle(color = Color.White, fontSize = 16.sp)
+                )
+            }
+        }
+    }
+}
+
+
